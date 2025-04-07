88f657f1aa6a464c699c76d86c2a101969515161
andi.berard <andi.berard@epitech.eu>
2025-04-07 22:12:56 +0200

    on s'en ballec des commit

---
+++ src/utils/KeyAssets.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2025
+** ARCADE_GFN_MAT
+** File description:
+** KeyAssets
+*/
+
+#pragma once
+
+namespace arcade {
+    enum class KeyAsset {
+        EMPTY,
+        snake_head,
+        snake_segment,
+        mine,
+        flag,
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight
+    };
+};
